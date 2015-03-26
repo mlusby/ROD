d3aa7eab26941ddff63fe1c4301d0de78db22df3
Shannon Barrett <shannon.barrett@dealertrack.com>
2015-03-26 16:37:07 -0500

    created initial git interface

---
+++ git_interface.js
@@ -0,0 +1,26 @@
+﻿var repo_location = "C:/git/Clickmotive";
+
+function GitShell(cmd, repo, callback) {
+    var exec = require('child_process').exec,
+        command = exec(cmd,{cwd: repo_location}),
+        result = '';
+    command.stdout.on('data', function(data) {
+        result += data.toString();
+    });
+    command.on('close', function(code) {
+        return callback(result);
+    });
+}
+
+function GetBranches(repo, callback) {
+    GitShell("git branch --all", repo, function (result) {
+        //var re = new RegExp(/^\s*/, 'gmi');
+        var re = new RegExp('remotes/origin/', 'gmi');
+        result.replace(re, '');
+        var array = result.split("\n  ");
+        return callback(JSON.stringify(array));
+    });    
+}
+
+GetBranches(repo_location, function (array) { console.log(array); });
+
