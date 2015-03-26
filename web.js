a2592607622b11f2bf3ba9209a6c4f602befe5ec
yusuf.songur <yusuf.songur@dealertrack.com>
2015-03-26 17:56:30 -0500

    handlebars, pull & display story, pull & display branches

--- web.js
+++ web.js
@@ -4,11 +4,15 @@ var app = express();
 app.use(express.logger());
 app.use(express.static('public'));
 
-//var hbs = require('hbs');	//Hooray Handlebars!
+var hbs = require('hbs');  //Hooray Handlebars!
 
 app.set('view engine', 'html');
-//app.engine('html', hbs.__express);
+app.engine('html', hbs.__express);
 app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+   res.render('index', {});
+});
 
 app.get('/GetBranches', function(req, res) {
    res.json(["develop","master"]);
@@ -16,23 +20,23 @@ app.get('/GetBranches', function(req, res) {
 
 app.get('/GetStories', function(req, res) {
    res.json(
-   		[
-   		{	
-   			"StoryName" : "Widget",
-   			"Commits" : 40,
-   			"Users" : ["Mark", "Shannon", "Yusuf"],
-   			"LastModified" : "2015-03-15 12:00 p.m."
-   		},
-   		{	
-   			"StoryName" : "N/A",
-   			"Commits" : 3, 
-   			"Users" : ["Mark"],
-   			"LastModified" : "2015-03-15 06:15 aj:Widget.m."
-   		}
-   		]
-   	);
+         [
+         {  
+            "StoryName" : "Widget",
+            "Commits" : 40,
+            "Users" : ["Mark", "Shannon", "Yusuf"],
+            "LastModified" : "2015-03-15 12:00 p.m."
+         },
+         {  
+            "StoryName" : "N/A",
+            "Commits" : 3, 
+            "Users" : ["Mark"],
+            "LastModified" : "2015-03-15 06:15 aj:Widget.m."
+         }
+         ]
+      );
 });
 var port = process.env.PORT || 5000 ;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+   console.log("Listening on " + port);
 });
