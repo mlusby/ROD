9753efbead636f0a6c2de21fca17a62db6de04bb
Mark Lusby <mlusby@gmail.com>
2015-03-26 15:51:07 -0500

    Added basic templating

--- web.js
+++ web.js
@@ -4,11 +4,15 @@ var app = express();
 app.use(express.logger());
 app.use(express.static('public'));
 
-//var hbs = require('hbs');	//Hooray Handlebars!
+var hbs = require('hbs');	//Hooray Handlebars!
 
 app.set('view engine', 'html');
-//app.engine('html', hbs.__express);
+app.engine('html', hbs.__express);
 app.use(express.bodyParser());
+
+app.get('/', function(req, res) {
+   res.render('index', {});
+});
 
 app.get('/GetBranches', function(req, res) {
    res.json(["develop","master"]);
