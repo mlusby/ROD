727470ef11a36f9225d28b1514b7d858e3df693b
Shannon Barrett <shannon.barrett@dealertrack.com>
2015-03-27 10:16:24 -0500

    Build_git_interface - returning json now

--- git_interface.js
+++ git_interface.js
@@ -1,5 +1,5 @@
-﻿//var repoLocation = "C:/git/ROD";
-var repoLocation = "/Users/e002796/Documents/Git/ROD";
+﻿var repoLocation = "C:/git/ROD";
+//var repoLocation = "/Users/e002796/Documents/Git/ROD";
 var storyTag = new RegExp(/^[bBdD]-[0-9]{5}/);
 
 //git-whatchanged format info:  https://www.kernel.org/pub/software/scm/git/docs/git-whatchanged.html
@@ -17,14 +17,20 @@ function GitShell(cmd, repo, callback) {
     command.stdout.on('data', function(data) {
         result += data.toString();
     });
+    command.stderr.on('data', function(data) {
+        result += data.toString();        
+    });
+    if (command.error != null)
+    {
+        console.log('exec error: ' + command.error);
+    }
     command.on('close', function(code) {      
         return callback(result);
     });
 }
 
 function GetBranches(repo, callback) {
-    GitShell("git branch --all", repo, function (result) {
-        //var re = new RegExp(/^\s*/, 'gmi');
+    GitShell("git branch --all", repo, function (result) {        
         var re = new RegExp('remotes/origin/', 'gmi');
         result.replace(re, '');
         var array = result.split("\n  ");
@@ -36,31 +42,50 @@ function GetBranches(repo, callback) {
 
 function ProcessStories(result, callback){
     var filtered = result.replace(/^.+?\.{2}.+?\n|^:.*$\n|^:.*$|^\s*/gmi,"");
-    var storyArray = filtered.split("\n");
-    for (var i = 0; i < storyArray.lengh; i++)
-        {
-            var re = new RegExp(/^[bBdD]-[0-9]{5}/);
-            if (re.test(storyArray[i]))
+    var commitArray = filtered.split("\n");
+    var unmatchedCount = 0;
+    var storyList = [];     
+    for (i = 0; i < commitArray.length; i++)
+        {            
+            var re = new RegExp(/^[bBdD]-[0-9]{5}/);                       
+            if (re.test(commitArray[i]))
             {
-                console.log(storyArray[i].match(re));
+                var array = commitArray[i].split(",");
+                var storyName = commitArray[i].match(re);
+                // check if duplicate story
+                var duplicate = false;
+                for (s = 0; s < storyList.length; s++)
+                    {
+                        if (storyList[s].Name == storyName) { duplicate = true; }
+                    }
+                if (duplicate == false)
+                {
+                    var story = {Name: storyName[0], Description: array[0], Users: array[1], Date: array[2], Commits: 1};
+                    storyList.push(story);
+                }
+                else
+                {
+                    var story = {Name: storyName[0], Description: array[0], Users: array[1], Date: array[2], Commits: 1};
+                    storyList.push(story);
+                    //storyList.Name[storyName];
+                }
+
             }
-            //console.log(storyArray[i]);
-        }  
-  //  var filtered2 = filtered.replace(/master..release\n/gmi,"");
-    return callback(filtered);    
-}
+            //get unmatched
+            else
+            {
+                unmatchedCount++;
+            }
+        }
+        var summary = {Stories: storyList, Unmatched: unmatchedCount};
+        return callback(JSON.stringify(summary));
+    }
 
 function GetStories(repo, branch, diffbranch, callback) {
     GitShell("git whatchanged --oneline --format=format:\"" + gitFormat +"\"" + branch + ".." + diffbranch, repo, function (result) {        
         ProcessStories(result, function(result){
             return callback(result);
         });
-        //console.log(result);
-        //var re = new RegExp(/^\s*/, 'gmi');        
-        //var re = new RegExp('remotes/origin/', 'gmi');
-        //result.replace(re, '');
-        //var array = result.split("\n  ");
-        //return callback(JSON.stringify(clean));
     });    
 }
 
