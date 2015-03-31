694d36b763cfd86f5f9b5f45434499a4563633a2
yusuf.songur <yusuf.songur@dealertrack.com>
2015-03-31 16:41:31 -0500

    fix refresh, and did some clean up on my mess

--- public/scripts/javascript/rodControll.js
+++ public/scripts/javascript/rodControll.js
@@ -1,5 +1,5 @@
-var initRet;
-var testStory
+//var initRet;
+//var testStory
 $(function(){
 	//pulls branches to select from first thing
 	pullBranches();
@@ -48,25 +48,46 @@ $(function(){
 		}*/
 		});
 	}
+
 	function popStory(data){
-		console.log('popStory');
+		$('div.story-container')
+			.hide()
+			.find('.story-container-body')
+			.html('');
+		//console.log('popStory');
 		var stories = data.Stories;
-		testStory = stories;
-		var retMarkup = data;
-		for(var i=0;i<stories.length;i++){
-			retMarkup = '<div class="panel panel-primary"><div class="panel-heading">'+stories[i].StoryName+', Commits: '+stories[i].Commits+'<i class="glyphicon glyphicon-print pull-right"></i><i data-toggle="modal" data-target="#story-modal" class="glyphicon glyphicon-new-window pull-right"></i></div>';
-			retMarkup += '<div class="panel-body"><ul>';
-			for(var k=0;k<stories[i].Users.length;k++){
-				retMarkup += '<li>'+stories[i].Users[k]+'</li>';
+		//testStory = stories;
+		var retMarkup = '';
+		if(stories.length != undefined && stories.length > 0){
+			for(var i=0;i<stories.length;i++){
+				retMarkup = '<div class="panel panel-primary"><div class="panel-heading">'+stories[i].StoryName+', Commits: '+stories[i].Commits+'<i class="glyphicon glyphicon-print pull-right"></i><i data-toggle="modal" data-target="#story-modal" class="glyphicon glyphicon-new-window pull-right"></i></div>';
+				retMarkup += '<div class="panel-body"><ul>';
+				for(var k=0;k<stories[i].Users.length;k++){
+					retMarkup += '<li>'+stories[i].Users[k]+'</li>';
+				}
+				var dateArray = stories[i].LastModified.split(/-|\s|:/);
+				//"2015-03-27 14:42:38 -0500"
+				var date = new Date(dateArray[0],dateArray[1]-1,dateArray[2],dateArray[3],dateArray[4]);
+				retMarkup += '</ul></div><div class="panel-footer">Last Modifed: '+date.toDateString() + ' ' + date.toLocaleTimeString()+'</div></div>';
+				$(retMarkup).appendTo('div.story-container-body');
+
+				if(i == stories.length-1){
+					$('div.story-container')
+						.find('h1 span')
+						.removeClass('text-danger')
+						.addClass('text-success')
+						.text(stories.length);
+					$('div.story-container').toggle('blind');
+				}
 			}
-			var dateArray = stories[i].LastModified.split(/-|\s|:/);
-			//"2015-03-27 14:42:38 -0500"
-			var date = new Date(dateArray[0],dateArray[1]-1,dateArray[2],dateArray[3],dateArray[4]);
-			retMarkup += '</ul><div class="panel-footer">Last Modifed: '+date.toDateString() + ' ' + date.toLocaleTimeString()+'</div></div>';
-			$(retMarkup).appendTo('div.story-container');
+		} else{
+			$('div.story-container')
+				.find('h1 span')
+				.removeClass('text-success')
+				.addClass('text-danger')
+				.text(0);
+			$('div.story-container').toggle('blind');
 		}
-		$('div.story-container').toggle('blind');
-		setTimeout(function(){window.scroll(0,$('#story').offset().top)},300);
 	}
 
 	function oops(){
